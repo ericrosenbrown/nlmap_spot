1dcadc75cf34a21afa135ad9a84d1ddf0d29205b
eric <eric_rosen@brown.edu>
2023-02-28 11:52:24 -0500

    pointcloud_classify_top_k now has bounding box based on estimated size of object

--- pointcloud_classify_top_k.py
+++ pointcloud_classify_top_k.py
@@ -6,18 +6,19 @@ import math
 from queue import PriorityQueue
 from tqdm import tqdm
 import open3d as o3d
-from spot_utils.utils import pixel_to_vision_frame
+from spot_utils.utils import pixel_to_vision_frame, pixel_to_vision_frame_depth_provided
+
 
 #################################################################
 # Hyperparameters and general initialization
-cache_images = False #load image cache if available, make image cache when needed
-cache_text = False#same but for text
+cache_images = True #load image cache if available, make image cache when needed
+cache_text = True#same but for text
 vis_boxes = False #show image with detected bounding boxes
 vis_details = False #show details for each bounding box
 headless = False #no visualization at all
 top_k = 5 #top k scores for models get stored
 img_dir_root_path = "./data/"
-img_dir_name = "spot-depth-color-pose-data3"
+img_dir_name = "cit121"
 img_dir_path = img_dir_root_path + img_dir_name
 cache_path = "./cache/"
 saved_model_dir = "./vild/image_path_v2"
@@ -46,7 +47,7 @@ numbered_category_indices = {cat['id']: cat for cat in numbered_categories}
 # Assuming point cloud already exists at location, load it in along with pose data
 pcd = o3d.io.read_point_cloud(img_dir_root_path+img_dir_name+"/pointcloud.pcd")
 pose_dir = pickle.load(open(img_dir_root_path+img_dir_name+"/"+pose_data_fname,"rb"))
-
+o3d.visualization.draw_geometries([pcd])
 #################################################################
 # Preprocessing categories and get params
 '''
@@ -58,7 +59,9 @@ category_name_string = ';'.join(['flipflop', 'street sign', 'bracelet',
 '''
 #category_name_string = "Table; Chair; Sofa; Lamp; Rug; Television; Fireplace; Pillow; Blanket; Clock; Picture frame; Vase; Lampshade; Candlestick; Books; Magazines; DVD player; CD player; Record player; Video game console; Board game; Card game; Chess set; Backgammon set; Carpet; Drapes; Blinds; Shelving unit; Side table; Coffee table; Footstool; Armchair; Bean bag; Desk; Office chair; Computer; Printer; Scanner; Fax machine; Telephone; Cell phone; Rug; Trash can; Wastebasket; Vacuum cleaner; Broom; Dustpan; Mop; Bucket; Dust cloth; Cleaning supplies; Iron; Ironing board; Hair dryer; Curling iron; Toilet brush; Towels; Soap; Shampoo; Toothbrush; Toothpaste; Razor; Shaving cream; Deodorant; Hairbrush; Hair ties; Makeup; Nail polish; Perfume; Cologne; Laundry basket; Clothes hanger; Closet; Dresser; Bed; Mattress; Pillows; Sheets; Blanket; Comforter; Quilt; Bedspread; Nightstand; Alarm clock; Lamp; Lamp; Rug"
 #category_name_string = "Hairbrush; Lamp; Chair; Sofa; Books; Television" 
-category_name_string = "Boxes; Hairbrush; Lamp; Chair; Sofa; Books; Television" 
+#category_name_string = "Boxes; Books; Monitor; Lamp" 
+category_name_string = "Cup; Chair; Boxes; Door; Table; Picture; Something to sit on " 
+
 #category_name_string = "food; chair; person; sofa; pillow; table; book"
 
 category_names = [x.strip() for x in category_name_string.split(';')]
@@ -305,14 +308,25 @@ for category_name in category_names:
 		center_x = int((xmin + xmax)/2.0)
 
 		transformed_point,bad_point = pixel_to_vision_frame(center_y,center_x,depth_img,rotation_matrix,position)
+		side_pointx,_ = pixel_to_vision_frame_depth_provided(center_y,xmax,depth_img[center_y,center_x],rotation_matrix,position)
+		side_pointy,_ = pixel_to_vision_frame_depth_provided(ymax,center_x,depth_img[center_y,center_x],rotation_matrix,position)
+
+		#TODO: what should bb_size be for z? Right now, just making it same as x. Also needs to be axis aligned
+		bb_sizex = np.linalg.norm(transformed_point-side_pointx)[0]*2
+		bb_sizey = np.linalg.norm(transformed_point-side_pointy)[0]*2
+		
+
 
 		if bad_point:
 			print(f"0 depth at the point for item {k} next bounding box")
 		else:
 			print(f"item {k} good inside {top_k_item_clip[1][0]}")
-			#bb = o3d.geometry.OrientedBoundingBox(center=np.array(transformed_point),R=np.array([[1,0,0],[0,1,0],[0,0,1]]), extent=np.array([1,1,1]))
-			bb = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1,origin=transformed_point)
+			print(bb_sizex,bb_sizey)
+			bb = o3d.geometry.OrientedBoundingBox(center=np.array(transformed_point),R=np.array([[1,0,0],[0,1,0],[0,0,1]]), extent=np.array([bb_sizex,bb_sizex,bb_sizey]))
+			bb.color = [1,0,0]
+			axis_center = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.5,origin=transformed_point)
 			top_axes.append(bb)
+			top_axes.append(axis_center)
 
 	if not headless:
 		plt.show()
